2c02f5f2eecac289e9d0fa83672254769496baf9
octo <allblvnk@gmail.com>
2020-08-19 14:31:52 +0100

    Version bump

--- src/Main.cs
+++ src/Main.cs
@@ -16,7 +16,7 @@ namespace AudicaModding
             public const string Name = "TimingAssist"; // Name of the Mod.  (MUST BE SET)
             public const string Author = "octo & alternity"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "1.1.1"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
 
