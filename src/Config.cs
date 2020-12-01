434c78a4464fa6b68fd5a87d15431eeaa9adfe5f
octo <allblvnk@gmail.com>
2020-12-01 11:15:26 +0000

    Rewrote a major part of it. Updated for modsettings

---
+++ src/Config.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System.Reflection;
+
+namespace TimingAssist
+{
+    public static class Config
+    {
+        public const string Category = "TimingAssist";
+
+        public static bool Enabled;
+
+        public static float TimingAssistAmount;
+
+
+        public static void RegisterConfig()
+        {
+            MelonPrefs.RegisterBool(Category, nameof(Enabled), true, "Enables the mod.");
+
+            MelonPrefs.RegisterFloat(Category, nameof(TimingAssistAmount), 1.0f, "Limits the timing window [0,1,0.05,1] {P}");
+
+            OnModSettingsApplied();
+        }
+
+        public static void OnModSettingsApplied()
+        {
+            foreach (var fieldInfo in typeof(Config).GetFields(BindingFlags.Static | BindingFlags.Public))
+            {
+                if (fieldInfo.FieldType == typeof(bool))
+                    fieldInfo.SetValue(null, MelonPrefs.GetBool(Category, fieldInfo.Name));
+                
+                if (fieldInfo.FieldType == typeof(float))
+                    fieldInfo.SetValue(null, MelonPrefs.GetFloat(Category, fieldInfo.Name));
+            }
+        }
+    }
+}
--- src/Main.cs
+++ src/Main.cs
@@ -1,121 +1,57 @@
 ﻿using System;
-using System.Collections;
 using MelonLoader;
 using UnityEngine;
 using Harmony;
+using TimingAssist;
 
-namespace AudicaModding
+
+[assembly: MelonGame("Harmonix Music Systems, Inc.", "Audica")]
+[assembly: MelonInfo(typeof(TimingAssistMod), "Timing Assist", "1.1.2", "octo & Alternity", "https://github.com/octoberU/TimingAssist")]
+
+public class TimingAssistMod : MelonMod
 {
-    public class AudicaMod : MelonMod
+    private static float percent => Config.TimingAssistAmount;
+
+    [HarmonyPatch(typeof(AudioDriver), "StartPlaying", new Type[0])]
+    private static class SetCueTimingWindow
     {
-        private static float percent = 1.0f;
-        private static string selectedSong;
-        private bool sliderExists = false;
-        public static class BuildInfo
+        private static void Postfix(AudioDriver __instance)
         {
-            public const string Name = "TimingAssist"; // Name of the Mod.  (MUST BE SET)
-            public const string Author = "octo & alternity"; // Author of the Mod.  (Set as null if none)
-            public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "1.1.1"; // Version of the Mod.  (MUST BE SET)
-            public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
-        }
+            if (!Config.Enabled) return;
+            SongCues.Cue[] cues = SongCues.I.GetCues();
+            SongList.SongData song = SongList.I.GetSong(SongDataHolder.I.songData.songID);
+            SongList.SongData.TempoChange[] tempos = song.tempos;
 
-        public OptionsMenuSlider TimingAssistSlider = new OptionsMenuSlider();
-
-        public static void OnSelect(SongSelectItem button)
-        {
-            string songID = button.mSongData.songID;
-            selectedSong = songID;
-        }
-
-        public static void PlaySong()
-        {
-            MelonCoroutines.Start(ChangeTimingWindow());
-        }
-
-        public void UpdateSlider()
-        {
-            if (this.TimingAssistSlider == null)
+            for (int i = 0; i < tempos.Length; i++)
             {
-                return;
-            }
-            else
-            {
-                if(percent > 1.0f)
+                float timingWindowMs = 200 * Mathf.Lerp(0.07f, 1.0f, percent);
+
+                float ticks = timingWindowMs / (60000 / (tempos[i].tempo * 480));
+                float halfTicks = ticks / 2;
+
+                for (int j = 0; j < cues.Length; j++)
                 {
-                    percent = 1.0f;
-                }
-                if (percent < 0.0f)
-                {
-                    percent = 0.0f;
-                }
-                this.TimingAssistSlider.label.text = "Timing Assist " + (percent * 100).ToString("F") + "%";
-            }
-        }
-        static IEnumerator ChangeTimingWindow()
-        {
-            if (percent < 1.0f)
-            {
-                yield return new WaitForSeconds(5);
-
-                SongCues.Cue[] cues = SongCues.I.GetCues();
-                SongList.SongData song = SongList.I.GetSong(selectedSong);
-                SongList.SongData.TempoChange[] tempos = song.tempos;
-
-                for (int i = 0; i < tempos.Length; i++)
-                {
-                    float timingWindowMs = 200 * Mathf.Lerp(0.07f, 1.0f, percent);
-
-                    float ticks = timingWindowMs / (60000 / (tempos[i].tempo * 480));
-                    float halfTicks = ticks / 2;
-
-                    for (int j = 0; j < cues.Length; j++)
+                    if (cues[j].behavior != Target.TargetBehavior.Chain && cues[j].behavior != Target.TargetBehavior.Dodge && cues[j].behavior != Target.TargetBehavior.Melee)
                     {
-                        if (cues[j].behavior != Target.TargetBehavior.Chain && cues[j].behavior != Target.TargetBehavior.Dodge && cues[j].behavior != Target.TargetBehavior.Melee)
+                        void UpdateTarget(SongCues.Cue cue)
                         {
-                            void UpdateTarget(SongCues.Cue cue)
+                            cue.slopAfterTicks = halfTicks;
+                            cue.slopBeforeTicks = halfTicks;
+                        }
+                        if (cues[j].tick >= tempos[i].tick)
+                        {
+                            if (tempos.Length >= tempos.Length + 1 && cues[j].tick < tempos[i + 1].tick)
                             {
-                                cue.slopAfterTicks = halfTicks;
-                                cue.slopBeforeTicks = halfTicks;
+                                UpdateTarget(cues[j]);
                             }
-                            if (cues[j].tick >= tempos[i].tick)
+                            else if (tempos.Length < tempos.Length + 1)
                             {
-                                if (tempos.Length >= tempos.Length + 1 && cues[j].tick < tempos[i + 1].tick)
-                                {
-                                    UpdateTarget(cues[j]);
-                                }
-                                else if (tempos.Length < tempos.Length + 1)
-                                {
-                                    UpdateTarget(cues[j]);
-                                }
+                                UpdateTarget(cues[j]);
                             }
                         }
                     }
                 }
             }
-
-
-        }
-        public override void OnUpdate()
-        {
-            if(OptionsMenu.I != null)
-            {
-                if(!sliderExists & OptionsMenu.I.mPage == OptionsMenu.Page.Gameplay)
-                {
-                    {
-                        OptionsMenu.I.AddHeader(0, "TimingAssist");
-
-                        this.TimingAssistSlider = OptionsMenu.I.AddSlider(0, "Timing Assist", "P", new Action<float>((float n) => { percent += (n * 0.05f); MelonModLogger.Log(percent.ToString()); UpdateSlider(); }), null, null, "Controls the size of the timing window");
-                        this.TimingAssistSlider.label.text = "Timing Assist " + (percent * 100).ToString("F") + "%";
-
-                        sliderExists = true;
-                    }
-                }
-                if (sliderExists & OptionsMenu.I.mPage != OptionsMenu.Page.Gameplay)
-                {
-                    sliderExists = false;
-                }
-            }      
         }
     }
 }
